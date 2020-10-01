f7d576d0529e734113815f66aef732978109ea3f
camiloleon96 <38619701+camiloleon96@users.noreply.github.com>
2020-10-01 09:16:47 +0200

    Add files via upload

---
+++ MyBank/BankAccount.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyBank
+{
+    public class BankAccount
+    {
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { 
+            get
+            {
+                decimal Balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    Balance += item.Amount;
+                }
+                return Balance;
+            }
+
+
+        }
+        private static int accountNumberSeed = 12345;
+
+        private List<Transaction> allTransactions = new List<Transaction>();
+
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+        }
+
+
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
+        }
+
+
+
+
+    }
+}
---
+++ MyBank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace MyBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("milo",1000);
+            var account2 = new BankAccount("timon",500);
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            Console.WriteLine($"the account {account.Number} that belongs to {account.Owner} has a balance of {account.Balance}");
+            Console.WriteLine($"the account {account2.Number} that belongs to {account2.Owner} has a balance of {account2.Balance}");
+        }
+    }
+}
